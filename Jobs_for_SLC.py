951c28f45acb7f2e3c207dddc127372b9095a38b
ben <btaylor280@gmail.com>
2017-09-07 15:51:09 -0500

    cleaning code

--- Jobs_for_SLC.py
+++ Jobs_for_SLC.py
@@ -9,15 +9,11 @@ path_to_Chrome = 'C:\Python27\Scripts\Seleniurm\chromedriver.exe'
 driver = webdriver.Chrome(path_to_Chrome)
 driver.implicitly_wait(30)
 
-
 url = "https://www.ksl.com/jobs/search/jobtype/full_time/posted/last_24_hours/city/salt+lake+city/state/UT/page/1"
 driver.get(url)
 sleep(3)
-
 nextpage = driver.find_element_by_xpath('//*[@title="Go to last page"]')
-
 nextpage = int(nextpage.text)
-
 
 for x in xrange(1, nextpage):
     url = ("https://www.ksl.com/jobs/search/jobtype/full_time/posted/last_24_hours/city/salt+lake+city/state/UT/page/" + str(x))
